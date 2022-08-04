95fedfe955eb77b5120c3862e5bd93103c0e3b8b
chenxianduan <1140387046@qq.com>
2022-08-04 15:33:49 +0800

    完成

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,19 +1,41 @@
-const from = layui.from
+const form = layui.form
+
 form.verify({
   nickname: (val) => {
     if (val.length > 6) return "昵称长度必须在 1 ~ 6 个字符之间！";
   },
-  email:[/@/,'邮箱格式输入错误'],
+  email: [/@/, '邮箱格式输入错误'],
 });
+
 const initUserInfo = () => {
   $.ajax({
     type: "GET",
     url: "/my/userinfo",
     success: (res) => {
       if (res.status !== 0) return layer.msg("获取用户信息失败！");
-      console.log(res);
+      form.val('formUserInfo', res.data)
     },
   });
 }
 
-initUserInfo  ()
+initUserInfo()
+
+$('#resetBtn').click(function (e) {
+  e.preventDefault()
+  initUserInfo()
+})
+
+$('.layui-form').submit(function (e) {
+  e.preventDefault()
+  $({
+    type: "POST",
+    url: "/my/userinfo",
+    data: form.val('formUserInfo'),
+    success: res => {
+      const { status, message } = res
+      if (status !== 0) return layer.msg("message");
+      // 调用父页面渲染函数
+      window.parent.getUserInfo();
+    },
+  })
+})

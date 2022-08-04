61e244137cfc49521b442bcfcfa8ac73ed9b6a7c
chenxianduan <1140387046@qq.com>
2022-08-04 19:38:08 +0800

    文章

--- assets/js/baseAPL.js
+++ assets/js/baseAPL.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://www.liulongbin.top:3007'
+const baseUrl = 'http://big-event-api-t.itheima.net'
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
